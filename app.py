0b593a58bbd8dd9bc8748ea5d7486d456ee2c8c7
miki <chiarichan.29@gmail.com>
2026-04-13 20:00:37 +0900

    商品登録と一覧表示機能を追加

--- app.py
+++ app.py
@@ -1,10 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+import sqlite3
 
 app = Flask(__name__)
 
+# DB接続
+def get_db():
+    conn = sqlite3.connect("database.db")
+    conn.row_factory = sqlite3.Row
+    return conn
+
+# 一覧表示
 @app.route("/")
 def index():
-    return render_template("index.html")
+    conn = get_db()
+    items = conn.execute("SELECT * FROM items").fetchall()
+    conn.close()
 
+    return render_template("index.html", items=items)
+
+# 商品追加
+@app.route("/add_item", methods=["POST"])
+def add_item():
+    name = request.form["name"]
+
+    conn = get_db()
+    conn.execute(
+        "INSERT INTO items (name, stock_quantity) VALUES (?, 0)",
+        (name,)
+    )
+    conn.commit()
+    conn.close()
+
+    return redirect("/")
+
+# サーバー起動
 if __name__ == "__main__":
     app.run(debug=True)
