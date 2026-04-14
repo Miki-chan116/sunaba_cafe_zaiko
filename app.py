d0571752fc3d186dd06397cfce38433bacc378d9
miki <chiarichan.29@gmail.com>
2026-04-14 20:20:27 +0900

    ログイン機能とログアウト機能を追加

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, url_for
 import sqlite3
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
+app.secret_key = "secret_key"
 
 # DB接続
 def get_db():
@@ -9,6 +11,36 @@ def get_db():
     conn.row_factory = sqlite3.Row
     return conn
 
+def init_user_db():
+    conn = get_db()
+
+    conn.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT UNIQUE,
+        password TEXT
+    )
+    """)
+
+    conn.commit()
+    conn.close()
+
+
+# 👇ここに追加！
+def create_user(username, password):
+    conn = get_db()
+
+    hashed_pw = generate_password_hash(password)
+
+    conn.execute(
+        "INSERT INTO users (username, password) VALUES (?, ?)",
+        (username, hashed_pw)
+    )
+
+    conn.commit()
+    conn.close()
+
+    
 
 # =========================
 # 一覧表示
@@ -144,9 +176,32 @@ def history():
 
     return render_template("history.html", logs=logs)
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        conn = get_db()
+        user = conn.execute(
+            "SELECT * FROM users WHERE username = ?",
+            (username,)
+        ).fetchone()
+        conn.close()
+
+        if user and check_password_hash(user["password"], password):
+            session["user_id"] = user["id"]
+            return redirect(url_for("index"))
+        else:
+            return "ログイン失敗"
+
+    return render_template("login.html")
+
 
 # =========================
 # サーバー起動
 # =========================
 if __name__ == "__main__":
+    init_user_db()
+    # create_user("miki", "1234")  ←コメントアウト！！
     app.run(debug=True, port=5001)
